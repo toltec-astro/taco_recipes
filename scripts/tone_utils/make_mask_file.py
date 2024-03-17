e6f8b410d48cdff94c62b27e930059fc3037dfbf
toltec <toltec@clipa>
2024-03-17 13:02:43 +0000

    add dry run

--- scripts/tone_utils/make_mask_file.py
+++ scripts/tone_utils/make_mask_file.py
@@ -5,6 +5,7 @@ import numpy as np
 from astropy.table import Table
 from pathlib import Path
 from toltec_tone_utils import TlalocEtcDataStore
+from contextlib import nullcontext
 
 
 
@@ -17,8 +18,11 @@ if __name__ == "__main__":
     parser.add_argument("--n_tones", "-t", default=-1, help="number of tones", type=int)
     parser.add_argument("--head", '-p', help='number of tones to mask at head', default=0, type=int)
     parser.add_argument("--tail", '-q', help='number of tones to mask at tail', default=0, type=int)
+    parser.add_argument("--start", '-s', help='index of tones to start', default=None, type=int)
+    parser.add_argument("--end", '-e', help='index of tones to end', default=None, type=int)
     parser.add_argument("--invert", '-i', help='invert the mask', action='store_true')
-    parser.add_argument("--etc_dir", '-e', help='the tlaloc etc dir', default='~/tlaloc/etc', type=Path)
+    parser.add_argument("--etc_dir", help='the tlaloc etc dir', default='~/tlaloc/etc', type=Path)
+    parser.add_argument("--dry_run", '-d', help='do dry run', action='store_true')
 
     option = parser.parse_args()
 
@@ -35,10 +39,16 @@ if __name__ == "__main__":
 
     mask = np.ones((n_tones, )).astype(bool)
     if option.head > 0:
-        mask[:option.head] = 0
+        start = option.start or 0
+        mask[start:start + option.head] = 0
     if option.tail > 0:
-        mask[-option.tail:] = 0
+        end = option.end or n_tones
+        mask[end-option.tail:end] = 0
     if option.invert:
         mask = ~mask
+    ctx = tlaloc.dry_run if option.dry_run else nullcontext
+    with ctx():
+        tlaloc.write_targ_mask_table(mask, nw=nw)
+    logger.info(f'mask: {mask.sum()} / {len(mask)}')
 
-    tlaloc.write_targ_mask_table(mask, nw=nw)
+
